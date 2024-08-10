f561c6eb1f60c38f37448e018af517d8a3b23e85
Dan Williams <dan@dotfive.co.uk>
2024-08-10 14:50:04 +0100

    Added standard linting configuration

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,30 @@
 //! the standard integer types.
 
 
+
+//		Global configuration
+
+#![cfg_attr(feature = "reasons", feature(lint_reasons))]
+
+//	Customisations of the standard linting configuration
+#![cfg_attr(    feature = "reasons",  allow(clippy::items_after_test_module, reason = "Not needed with separated tests"))]
+#![cfg_attr(not(feature = "reasons"), allow(clippy::items_after_test_module))]
+
+//	Lints specifically disabled for unit tests
+#![cfg_attr(test, allow(
+	clippy::cognitive_complexity,
+	clippy::exhaustive_enums,
+	clippy::exhaustive_structs,
+	clippy::expect_used,
+	clippy::indexing_slicing,
+	clippy::let_underscore_untyped,
+	clippy::missing_assert_message,
+	clippy::missing_panics_doc,
+	clippy::must_use_candidate,
+	clippy::panic,
+	clippy::print_stdout,
+	clippy::unwrap_in_result,
+	clippy::unwrap_used,
+))]
+
+
